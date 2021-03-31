662e359c4fe48cc4007b6c05ac5be64c728b1ac7
RReiso <ruta.littlemore@gmail.com>
2021-03-30 23:23:22 -0400

    Created navbar with Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+  resources :activities
+  get "/login", to: "home#login"
+  get "/signup", to: "home#signup"
+  # get "/login", controller: "home", action: "login"
 end
