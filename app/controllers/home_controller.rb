79382b1ec1a30b449ba2e5d2ddc297d90b946397
RReiso <ruta.littlemore@gmail.com>
2021-03-31 21:53:05 -0400

    added search bar and created a flash message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,14 @@ class HomeController < ApplicationController
   def index
   end
 
+  def search
+    
+if params[:search].blank?  
+    redirect_to(root_path, alert: "Empty search field!") and return  
+  else  
+
+  end  
+
+  end
  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  get "/search", to: "home#search", as: :search_path
   get "/signup", to: "users#new", as: :sign_up
   get    "/login", to: "sessions#new", as: :log_in
   post   "/login", to: "sessions#create"
