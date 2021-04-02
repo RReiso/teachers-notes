7e355f8fa83366634622a590d23d28c40f3f9f0b
RReiso <ruta.littlemore@gmail.com>
2021-04-01 22:00:45 -0400

    render error messages with partial from back end when user signs up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,15 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    
   end
 
   def create
-    # render plain: params
+    @user = User.create(user_params)
+    render "new"
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
