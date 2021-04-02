2de4f11e3a82b0c02e469a75a7e23709e1a73873
RReiso <ruta.littlemore@gmail.com>
2021-04-01 20:52:35 -0400

    validate if password and password_confirmation match with JS

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,8 +1,13 @@
 console.log("hello");
-window.checkForm = function(form) {
-	if (form.user[password].value !== form.user[password_confirmation].value) {
+window.checkForm = function (form) {
+	// console.log(form["user[password]"].value);
+	if (
+		form["user[password]"].value !== form["user[password_confirmation]"].value
+	) {
 		alert("Error: Passwords do not match!");
-		form.user[password].focus();
+		form["user[password]"].value = "";
+		form["user[password_confirmation]"].value = "";
+		form["user[password]"].focus();
 		return false;
 	}
-}
+};
