fdf09b6080da6df6f792a037b7e2a737700321e8
RReiso <ruta.littlemore@gmail.com>
2021-04-09 13:50:24 -0400

    Edit form for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
-before_action :find_user_by_user_id, only: %i[index create destroy]#in application_controller
-	before_action :find_activity_by_activity_id, only: %i[index create destroy]#private
+  before_action :no_access, only: %i[show]
+before_action :find_user_by_user_id, only: %i[index create edit destroy]#in application_controller
+	before_action :find_activity_by_activity_id, only: %i[index create edit destroy]#private
+	before_action :find_comment_by_id, only: %i[edit]#private
 
   def index
     @comments = Comment.where(activity_id: params[:activity_id])
@@ -12,15 +14,22 @@ before_action :find_user_by_user_id, only: %i[index create destroy]#in applicati
     redirect_to user_activity_comments_path(@user, @activity, anchor: 'comments')
   end
  
+  def edit
+
+  end
 
   def destroy
 		Comment.find(params[:id]).destroy
 		flash[:success] = 'Comment deleted!'
-		redirect_to user_activity_comments_path(@user, @activity, anchor: 'comments')
+		redirect_to user_activity_comments_path(@user, @activity)
 	end
 
   private
   def find_activity_by_activity_id
     @activity = Activity.find(params[:activity_id])
+  end
+
+  def find_comment_by_id
+    @comment = Comment.find(params[:id])
   end
 end
